3747105eab952d76e9d98f482f060c7556a0a600
BLew1 <36410974+BLew1@users.noreply.github.com>
2018-02-21 09:38:07 -0500

    Add files via upload

---
+++ Variables/16/NameAgeSalary.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class NameAgeSalary{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("What's your name? ");
+        String name = input.next();
+        
+        System.out.print("Hi " + name + "! How old are you? ");
+        int age = input.nextInt();
+        
+        System.out.print("So you're " + age + ", eh? That's not old at all!");
+        System.out.print("How much do you make, " + name);
+        double salary = input.nextDouble();
+        
+        System.out.print(salary + "! I hope that's per hour and not per year! LOL!");
+    }
+}
