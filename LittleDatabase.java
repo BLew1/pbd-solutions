32d24ddf619d7aa261a9e4e86a61794246898fc2
BLew1 <36410974+BLew1@users.noreply.github.com>
2018-03-26 09:33:44 -0400

    Update LittleDatabase.java

--- LittleDatabase.java
+++ LittleDatabase.java
@@ -7,37 +7,35 @@ class Student{
 public class LittleDatabase {
     public static void main(String[] args){
         Student[] list1 = new Student[3];
-        Student student1 = new Student();
-        Student student2 = new Student();
-        Student student3 = new Student();
+        list1[0] = new Student();
+        list1[1] = new Student();
+        list1[2] = new Student();
         Scanner input = new Scanner(System.in);
 
         System.out.print("Enter the first student's name: ");
-        student1.name = input.nextLine();
+        list1[0].name = input.nextLine();
         System.out.print("Enter the first student's grade: ");
-        student1.grade = input.nextInt();
+        list1[0].grade = input.nextInt();
         System.out.print("Enter the first student's average: ");
-        student1.average = input.nextDouble();
-        list1[0] = student1;
+        list1[0].average = input.nextDouble();
 
         System.out.print("Enter the second student's name: ");
-        student2.name = input.next();
+        list1[1].name = input.next();
         System.out.print("Enter the second student's grade: ");
-        student2.grade = input.nextInt();
+        list1[1].grade = input.nextInt();
         System.out.print("Enter the second student's average: ");
-        student2.average = input.nextDouble();
-        list1[1] = student2;
+        list1[1].average = input.nextDouble();
 
         System.out.print("Enter the third student's name: ");
-        student3.name = input.next();
+        list1[2].name = input.next();
         System.out.print("Enter the third student's grade: ");
-        student3.grade = input.nextInt();
+        list1[2].grade = input.nextInt();
         System.out.print("Enter the third student's average: ");
-        student3.average = input.nextDouble();
-        list1[2] = student3;
+        list1[2].average = input.nextDouble();
 
         System.out.println("The names are: " + list1[0].name + " " + list1[1].name + " " + list1[2].name);
         System.out.println("The grades are: " + list1[0].grade + " " + list1[1].grade + " " + list1[2].grade);
         System.out.println("The averages are: " + list1[0].average + " " + list1[1].average + " " + list1[2].average);
     }
 }
+
